c12a23448b9c635498e2f8d20e3697e941e8ebdd
Sean O'Neill <s.oneill@f5.com>
2021-03-23 14:37:12 +0000

    Add max connections config to TransportServer

--- internal/configs/transportserver.go
+++ internal/configs/transportserver.go
@@ -172,6 +172,7 @@ func generateStreamUpstream(upstream *conf_v1alpha1.Upstream, upstreamNamer *ups
 
 	name := upstreamNamer.GetNameForUpstream(upstream.Name)
 	maxFails := generateIntFromPointer(upstream.MaxFails, 1)
+	maxConnections := generateIntFromPointer(upstream.MaxConnections, 0)
 	failTimeout := generateTimeWithDefault(upstream.FailTimeout, "10s")
 
 	for _, e := range endpoints {
@@ -186,9 +187,10 @@ func generateStreamUpstream(upstream *conf_v1alpha1.Upstream, upstreamNamer *ups
 
 	if !isPlus && len(endpoints) == 0 {
 		upsServers = append(upsServers, version2.StreamUpstreamServer{
-			Address:     nginxNonExistingUnixSocket,
-			MaxFails:    maxFails,
-			FailTimeout: failTimeout,
+			Address:        nginxNonExistingUnixSocket,
+			MaxFails:       maxFails,
+			FailTimeout:    failTimeout,
+			MaxConnections: maxConnections,
 		})
 	}
 
